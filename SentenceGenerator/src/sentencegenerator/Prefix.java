faf9037d2c664bfd857f4ac9fd099d3066ec4143
Anthony Chan <anthony.me.chan@gmail.com>
2017-03-28 18:23:40 -0400

    add prefix class

--- SentenceGenerator/src/sentencegenerator/Prefix.java
+++ SentenceGenerator/src/sentencegenerator/Prefix.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sentencegenerator;
 
+import java.util.Vector;
+
 /**
- *
  * @author aychan
  */
 public class Prefix {
+    public Vector pref;
+    static final int MULTIPLIER = 31; 
+    
+    /**
+     * Constructor
+     * @param p 
+     */
+    Prefix(Prefix p){
+        pref = (Vector)p.pref.clone();
+    }
+    
+    /**
+     * Constructor
+     * @param n
+     * @param str 
+     */
+    Prefix(int n, String str){
+        pref = new Vector();
+        for(int i = 0 ; i < n ; i++){
+            pref.addElement(str);
+        }
+    }
+    
+    /**
+     * Hash for prefix words
+     * @return h 
+     */
+    public int hashCode(){
+        int h = 0;
+        for(int i = 0; i < pref.size(); i++){
+            h = MULTIPLIER*h + pref.elementAt(i).hashCode();
+        }
+        return h;
+    }
+    
+    /**
+     * compare prefixes for equal words
+     * @param o
+     * @return True if equal 
+     */
+    public boolean equals(Object o){ //
+        Prefix p = (Prefix)o;
+        for(int i = 0; i < pref.size(); i++){
+            if(!pref.elementAt(i).equals(p.pref.elementAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
     
 }
