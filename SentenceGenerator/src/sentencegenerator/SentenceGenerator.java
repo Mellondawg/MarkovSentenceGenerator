85e708e84e4f8ac1175b4ae73a0447a8fd01a3bf
Anthony Chan <anthony.me.chan@gmail.com>
2017-03-28 15:19:03 -0400

    create skeleton

---
+++ SentenceGenerator/src/sentencegenerator/SentenceGenerator.java
@@ -0,0 +1,16 @@
+package sentencegenerator;
+/**
+ *
+ * @author Anthony Chan, Ayodeji Marquis, John Melloni
+ */
+public class SentenceGenerator {
+
+    /**
+     * @param args the command line arguments
+     * Driver for UI aspects (case switch within infinite while loop, like in 242)
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
